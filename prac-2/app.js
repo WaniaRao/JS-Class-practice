1a653f6f6d8de2d416e17aa516a08fff0e7bf318
umaimarao <umaima.ra0000@gmail.com>
2026-09-21 05:28:28 -0700

    practice

--- prac-2/app.js
+++ prac-2/app.js
@@ -72,4 +72,84 @@ for (var i = 1; i <= lines; i++) {
 
     console.log(pattern);
     document.write(pattern + "<br>");
+// ------------------Nested Loops---------------------
+
+// pattern 1
+//******
+//******
+//******
+//******
+//******
+//****** 
+for (var i = 1; i <= 6; i++) {
+
+    var stars = "";
+
+    for (var j = 1; j <= 6; j++) {
+        stars = stars + "*";
+    }
+
+    console.log(stars);
+   
+}
+
+
+// pattern 2
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+for (var i = 1; i <= 6; i++) {
+    var stars = "";
+
+    for (var j = 1; j <= i; j++) {
+        stars = stars + "*";
+    }
+
+    console.log(stars);
+  
+}
+
+
+// pattern 3
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+for (var i = 6; i >= 1; i--) {
+    var stars = "";
+
+    for (var j = 1; j <= i; j++) {
+        stars = stars + "*";
+    }
+
+    console.log(stars);
+  
+}
+
+// Triangle pattern
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+var lines = +prompt("How many lines do you want?");
+
+for (var i = 1; i <= lines; i++) {
+
+    var pattern = "";
+
+    for (var j = 1; j <= lines - i; j++) {
+        pattern = pattern + "&nbsp&nbsp;";
+    }
+
+    for (var j = 1; j <= 2 * i - 1; j++) {
+        pattern = pattern + "*";
+    }
+
+    document.write(pattern + "<br>");
 }
